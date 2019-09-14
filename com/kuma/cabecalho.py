b7dc4bd246d6dd3886d6350a9812072857de55b9
Raissa Santos Reis <1900801@SALAS.AULAS>
2019-09-13 21:31:06 -0300

    top

--- com/kuma/cabecalho.py
+++ com/kuma/cabecalho.py
@@ -1,15 +1,9 @@
-print("*********************************")
-print("******Escolha o seu Jogo!"********)
-print("*********************************")
-
-print("(1) Forca (2) Adivinhação")
-
-jogo = int(input("Qual jogo? "))
-
-if (jogo == 1):
-print("Jogando Forca")
-    forca.jogar()
-elif (jogo == 2😞
-    print("Jogando Adivinhação")
-    adivinhacao.jogar()
+class NOMES():
+	def nome(self):
+		nom = 'DEVOPS'
+		if nom == 'DEVOPS':
+			return nom
+		else:
+			return False
+	
 	
---
+++ com/kuma/test/test_cabecalho.py
@@ -0,0 +1,10 @@
+from unittes import TestCase
+from com.kuma.cabecalho import NOMES
+
+class TestCabecalho(TestCase):
+	
+		def setUp(self):
+			self.cabecalho = NOMES()
+		
+		def test_nome(self):
+			self.assertEqual(self.cabecalho.nome(), 'DEVOPS')
